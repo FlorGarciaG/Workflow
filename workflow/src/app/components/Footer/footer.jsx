569892c260c7c50d804e62329e92b1dde370b3f8
flor <florcongar26@gmail.com>
2024-05-23 17:27:10 -0600

    Footer

--- workflow/src/app/components/Footer/footer.jsx
+++ workflow/src/app/components/Footer/footer.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import styles from './footer.module.css';
 
 const Footer = () => {
     return (
-        <div className={`d-flex justify-content-center align-items-center text-center p-3`}>
+        <div className={`d-flex justify-content-center align-items-center text-center ${styles.footer}`}>
             <Container>
-                <Row className="justify-content-center">
+                <Row className="justify-content-center m-3">
                     <Col md="auto" className={`d-flex align-items-center`}>
                         <p>
                             © QuickFix Inc.
                         </p>
                     </Col>
-                    <Col md="auto" className={`d-flex align-items-center`}>
-                        <p>
-                            Privacy Policy
-                        </p>
-                    </Col>
+                    <Row  className="justify-content-center">
+                        <Col md="auto" className={`d-flex align-items-center`}>
+                            <p>
+                                Privacy Policy
+                            </p>
+                        </Col>
+                        <Col md="auto" className={`d-flex align-items-center`}>
+                            <p>
+                                Terms
+                            </p>
+                        </Col>
+                        <Col md="auto" className={`d-flex align-items-center`}>
+                            <p>
+                                Copyright Policy
+                            </p>
+                        </Col>
+                    </Row>
                 </Row>
             </Container>
         </div>
