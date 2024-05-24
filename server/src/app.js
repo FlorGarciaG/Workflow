bc9b4bf0e73ab5f40a82795d8b98f67c37ea6e62
Yibran07 <yibran.perve@gmail.com>
2024-05-24 00:41:20 -0600

    auth.controller

--- server/src/app.js
+++ server/src/app.js
@@ -3,13 +3,14 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import multer from "multer";
-import fs from 'fs';
+import fs from "fs";
 
 import TaskRoutes from "./routes/task.routes.js";
 import UserRoutes from "./routes/user.routes.js";
 import SolutionRoutes from "./routes/solution.routes.js";
 import SolxpruRoutes from "./routes/solxpru.routes.js";
 import TestRoutes from "./routes/test.routes.js";
+import AuthRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -24,12 +25,19 @@ console.log(URI);
 app.set("port", process.env.PORT ? process.env.PORT : 5001);
 
 //Middlewares
-app.use(cors(`origin: "http://localhost:${process.env.PORT_FRONT}", credentials: true,`));
-console.log(`origin: "http://localhost:${process.env.PORT_FRONT}", credentials: true,`);
+app.use(
+  cors(
+    `origin: "http://localhost:${process.env.PORT_FRONT}", credentials: true,`
+  )
+);
+console.log(
+  `origin: "http://localhost:${process.env.PORT_FRONT}", credentials: true,`
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
 //Routes
+app.use("/api", AuthRoutes);
 app.use("/api/tasks", TaskRoutes);
 app.use("/api/users", UserRoutes);
 app.use("/api/solutions", SolutionRoutes);
---
+++ server/src/controllers/authController.js
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import crypto from "crypto";
+
+const prisma = new PrismaClient();
+
+export const login = async (req, res) => {
+  const { correo, contrasenia } = req.body;
+  try {
+    const usuarioFound = await prisma.Usuario.findUnique({
+      where: { correo: correo },
+    });
+    if (!usuarioFound) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+    const hash = crypto.createHash("sha256");
+    hash.update(contrasenia);
+    const hashedContrasenia = hash.digest("hex");
+
+    if (hashedContrasenia !== usuarioFound.contrasenia) {
+      return res.status(400).json({ message: "La contraseña es incorrecta" });
+    }
+
+    res.json({
+      id_usuario: usuarioFound.id,
+      nombre: usuarioFound.nombre,
+      ap_paterno: usuarioFound.ap_paterno,
+      ap_materno: usuarioFound.ap_materno,
+      correo: usuarioFound.nombreCompleto,
+      rol: usuarioFound.rol,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
