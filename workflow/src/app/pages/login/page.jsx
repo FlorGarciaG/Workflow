1132c86f921c80e5e0a8499656993e7d3fcea3df
flor <florcongar26@gmail.com>
2024-05-24 08:53:12 -0600

    direccion a dashboard

--- workflow/src/app/pages/login/page.jsx
+++ workflow/src/app/pages/login/page.jsx
@@ -1,23 +1,37 @@
 "use client";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import LoginForm from "../../components/loginForm/loginForm";
 import { Row, Col, Container } from "react-bootstrap";
-import { useAuth } from "@/app/context/AuthContext";
+import IncidenciasUser from "../user/incidenciasUser/page";
 
 function Login() {
+    const [storedUser, setStoredUser] = useState(null);
+
+    useEffect(() => {
+        const userFromStorage = localStorage.getItem("user");
+        if (userFromStorage) {
+            setStoredUser(JSON.parse(userFromStorage));
+        }
+    }, []);
 
     return (
         <>
-                <Container className="d-flex justify-content-center align-items-center">
-                    <div className="p-5">
-                        <Col className="m-4">
-                            <div className={` ${styles.formlogin}`}>
-                                <LoginForm />
-                            </div>
-                        </Col>
-                    </div>
-                </Container>
+            {storedUser !== null ? (
+                <IncidenciasUser/>
+            ) : (
+                <>
+                    <Container className="d-flex justify-content-center align-items-center">
+                        <div className="p-5">
+                            <Col className="m-4">
+                                <div className={` ${styles.formlogin}`}>
+                                    <LoginForm />
+                                </div>
+                            </Col>
+                        </div>
+                    </Container>
+                </>
+            )}
         </>
     )
 }
