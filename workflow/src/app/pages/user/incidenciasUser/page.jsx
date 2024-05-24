9acbc5405cb769577c2fa9f4c6275a619bc21781
RendonSofia1 <rendonsofi03@gmail.com>
2024-05-23 20:21:42 -0600

    incidentes-page

--- workflow/src/app/pages/user/incidenciasUser/page.jsx
+++ workflow/src/app/pages/user/incidenciasUser/page.jsx
@@ -1,23 +1,44 @@
 "use client";
 import React from "react";
 import styles from "./page.module.css";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Button, Card } from "react-bootstrap";
 
 function incidenciasUser() {
-    return (
-        <>
-            <Container>
-                <Row>
-                    <Col>
-                        <Button>
-                            Crear Incidencia
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    )
+  return (
+    <>
+      <Container className="my-4">
+        <Row className="mb-3">
+          <Col className="mb-3">
+            <Button className="">Crear Incidencia</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={4} md={4} sm={6} xs={12} className="mb-3">
+            <div className={` ${styles.card}`}>
+              <div className={`py-2 px-3 ${styles.cardTitle}`}>Titulo</div>
+              <div className={`p-3 ${styles.subCard}`}>
+              <div className={`mb-2 ${styles.cardSubtitle}`}>Descripcion</div>
+              <div className={styles.cardBody}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laboriosam, sed sit accusantium ex iste dolor, ea saepe
+                voluptatum fugit nostrum alias! Possimus fugiat distinctio
+                quibusdam. .
+              </div>
+              <hr />
+              <div className={`d-flex flex-wrap justify-content-between ${styles.btns}`}>
+                <button className={`mb-2 ${styles.btnD}`}>Detalles</button>
+                {/* <button className={styles.btn}>Asignar</button> */}
+                <button className={`mb-2 ${styles.btn}`}>Atención</button>
+              </div>
+              </div>
+            </div>
+          </Col>
+          
+          
+        </Row>
+      </Container>
+    </>
+  );
 }
-
 
 export default incidenciasUser;
