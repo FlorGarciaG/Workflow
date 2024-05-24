d47a4a3a7227cda82b8cb5dc1983519d74a5aafc
RendonSofia1 <rendonsofi03@gmail.com>
2024-05-23 18:35:28 -0600

    Home page
    missing navbar styles

--- workflow/src/app/page.js
+++ workflow/src/app/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Container } from "react-bootstrap";
 
 export default function Home() {
   return (
-    <main>
-      hola
-    </main>
+    <>
+      <div>
+        <div className= {` d-flex flex-column align-items-center justify-content-center ${styles.backg}`}>
+          <h1 className={styles.h}>Bienvenido a QuickFix, un sistema para control de incidencias</h1>
+        </div>
+      </div>
+    </>
   );
 }
