b08a81cf1579beda3088ac63923741d76eb7f603
Yibran07 <yibran.perve@gmail.com>
2024-05-23 19:22:49 -0600

    Yibran

--- server/src/app.js
+++ server/src/app.js
@@ -1,29 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
 
-import TaskRoutes from './routes/task.routes.js';
-import UserRoutes from './routes/user.routes.js';
+import TaskRoutes from "./routes/task.routes.js";
+import UserRoutes from "./routes/user.routes.js";
 
 const app = express();
 
-const URI = process.env.DATABASE_URL ? process.env.DATABASE_URL : 'mysql://root:vyJDrzOGquIrgoWUEuvvdgUBVcQfreWO@monorail.proxy.rlwy.net:21532/railway';
+dotenv.config();
+
+const URI = process.env.DATABASE_URL
+  ? process.env.DATABASE_URL
+  : "mysql://root:vyJDrzOGquIrgoWUEuvvdgUBVcQfreWO@monorail.proxy.rlwy.net:21532/railway";
 console.log(URI);
 
 //Settings
-app.set('port', process.env.PORT ? process.env.PORT : 5001);
-
+app.set("port", process.env.PORT ? process.env.PORT : 5001);
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //Routes
-app.use('/api/tasks', TaskRoutes);
-app.use('/api/users', UserRoutes);
-
-
+app.use("/api/tasks", TaskRoutes);
+app.use("/api/users", UserRoutes);
 
 //Export
 export default app;
