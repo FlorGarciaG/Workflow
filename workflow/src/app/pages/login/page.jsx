7dff9e66732696da3fd6ef9c706ea3952414df96
flor <florcongar26@gmail.com>
2024-05-24 06:04:27 -0600

    direccionamiento de login

--- workflow/src/app/pages/login/page.jsx
+++ workflow/src/app/pages/login/page.jsx
@@ -3,10 +3,16 @@ import React from "react";
 import styles from "./page.module.css";
 import LoginForm from "../../components/loginForm/loginForm";
 import { Row, Col, Container } from "react-bootstrap";
+import { useAuth } from "@/app/context/AuthContext";
+import IncidenciasUser from "../user/incidenciasUser/page";
 
 function Login() {
+    const { isAuthen, isLoading } = useAuth();
     return (
         <>
+        {isAuthen ? (
+            <IncidenciasUser />
+          ) : (
             <Container className="d-flex justify-content-center align-items-center">
                 <div className="p-5">   
                     <Col  className="m-4">
@@ -16,6 +22,7 @@ function Login() {
                     </Col>
                 </div>
             </Container>
+          )}
         </>
     )
 }
